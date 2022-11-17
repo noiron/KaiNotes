7d9587e631a5843cc7f14c6fd95b148c5753eeb3
wu kai <wukai2048@gmail.com>
2022-11-17 23:14:46 +0800

    can resize tag cloud

--- src/webview.ts
+++ src/webview.ts
@@ -6,29 +6,64 @@ export function getWebviewContent(tags: any[] = []) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cat Coding</title>
+    <style>
+      #buttons {
+        position: fixed;
+        right: 20px;
+        bottom: 20px;
+        width: 40px;
+        text-align: center;
+        top: 45%;
+      }
+      .button {
+        font-size: 32px;
+        font-weight: bold;
+        cursor: pointer;
+        color: #000;
+      }
+    </style>
     <script src="https://cdn.jsdelivr.net/npm/wordcloud@1.2.2/src/wordcloud2.min.js"></script>
 </head>
 <body>
     <canvas id="wordcloud"></canvas>
+    <div id="buttons">
+      <div class="button" onclick="redraw(1)">+</div>
+      <div class="button" onclick="redraw(-1)">-</div>
+      <div class="button" onclick="draw()">🔁</div>
+    </div>
+
     <script>
       const tags = ${JSON.stringify(tags)};
 
-      const canvas = document.getElementById('wordcloud');
-      canvas.width = window.innerWidth * 0.95;
-      canvas.height = window.innerHeight * 0.95;
+      let weightFactor = 5;
 
-      WordCloud(canvas, {
-      list: tags,
-      gridSize: 5,
-      fontFamily: 'Times, serif',
-      weightFactor: 10,
-      color: 'random-dark',
-      rotateRatio: 0,
-      rotationSteps: 2,
-      backgroundColor: '#fff',
-			minSize: 4,
-      drawOutOfBound: true
-    });
+      function redraw(diff) {
+        weightFactor += diff;
+        draw();
+      }
+  
+      const canvas = document.getElementById('wordcloud');
+  
+      function draw() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+  
+        WordCloud(canvas, {
+          list: tags,
+          gridSize: 5,
+          fontFamily: 'Times, serif',
+          weightFactor,
+          color: 'random-dark',
+          rotateRatio: 0,
+          rotationSteps: 2,
+          backgroundColor: '#fff',
+          minSize: 4,
+          drawOutOfBound: true
+        });
+      }
+  
+      draw();
+  
     </script>
 </body>
 </html>`;
