a36a65fad6611b95d1f44120d8d03f9e36001aa9
wu kai <wukai2048@gmail.com>
2022-11-26 10:41:28 +0800

    feat: 加入 tag 的输入补全

--- src/extension.ts
+++ src/extension.ts
@@ -8,8 +8,10 @@ import { ALL_TAGS, MARKDOWN_REGEX, UNTAGGED } from './constants';
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "kainotes" is now active!');
 
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+
   vscode.commands.registerCommand('kainotes.tagCloud', async function () {
-    const workspaceFolders = vscode.workspace.workspaceFolders;
+    // const workspaceFolders = vscode.workspace.workspaceFolders;
     if (!workspaceFolders) {
       return vscode.window.showInformationMessage('Open a folder first');
     }
@@ -75,8 +77,37 @@ export function activate(context: vscode.ExtensionContext) {
     });
   });
 
+  const completion = vscode.languages.registerCompletionItemProvider(
+    'markdown',
+    {
+      async provideCompletionItems(document, position) {
+        if (!workspaceFolders) {
+          return;
+        }
+        const folderUri = workspaceFolders[0].uri;
+        // 这里每次输入都重复调用了？之后优化
+        const list = await walk(folderUri);
+        const tags = await getTags(list);
+        const keys = Object.keys(tags);
+
+        return keys.map(
+          (tag) =>
+            new vscode.CompletionItem(
+              tag,
+              vscode.CompletionItemKind.Constant
+              // vscode.CompletionItemKind.Value
+            )
+        );
+      },
+    },
+    '#' // triggered whenever a '#' is being typed
+  );
+
+  context.subscriptions.push(completion);
+
   context.subscriptions.push(
-    vscode.window.onDidChangeActiveTextEditor(() => decorateTags())
+    vscode.window.onDidChangeActiveTextEditor(() => decorateTags()),
+    vscode.workspace.onDidChangeTextDocument(() => decorateTags())
   );
 
   decorateTags();
@@ -102,7 +133,7 @@ function decorateTags() {
     borderRadius: '4px',
     backgroundColor: '#d9ad00',
     color: '#1f1f1f',
-    fontWeight: 'bold',
+    fontWeight: 'medium',
   });
 
   editor.setDecorations(decorationType, ranges);
