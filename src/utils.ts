0b4d156cfe784a691b6322995853b38f89633df3
wu kai <wukai2048@gmail.com>
2023-02-13 10:38:40 +0800

    refactor: remove some unused code

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,9 @@
 import * as vscode from 'vscode';
 import { TextDecoder } from 'util';
-import * as path from 'path';
 import { posix } from 'path';
 import * as fs from 'fs';
 import { isMarkdownFile, extractFileTags } from 'kainotes-tools';
-import { ALL_TAGS, MARKDOWN_REGEX, UNTAGGED } from './constants';
+import { UNTAGGED } from './constants';
 
 /**
  * 给定一个文件地址，以字符串形式返回文件内容
@@ -71,57 +70,7 @@ export async function walk(folder: vscode.Uri): Promise<string[]> {
   return list;
 }
 
-// /**
-//  * 在文件列表中查找包含给定标签的文件
-//  * @param folderPath {string} 文件夹路径
-//  * @param fileList {string[]} 文件路径列表
-//  * @param searchTag {string} 要查找的标签
-//  * @returns
-//  */
-// export async function getFilesContainTag(
-//   folderPath: string,
-//   fileList: string[],
-//   searchTag: string
-// ) {
-//   const list: string[] = [];
-//   const promises = fileList.filter(isMarkdownFile).map(async (absolutePath) => {
-//     const tagsInFile = await extractFileTags(absolutePath);
-//     const relativePath = path.relative(folderPath, absolutePath);
 
-//     if (searchTag === ALL_TAGS) {
-//       list.push(relativePath);
-//     } else if (tagsInFile) {
-//       for (const tag of tagsInFile) {
-//         if (tag === searchTag) {
-//           list.push(relativePath);
-//           break;
-//         }
-//       }
-//     } else if (!searchTag) {
-//       // 查找所有不包含标签的文件
-//       list.push(relativePath);
-//     }
-//   });
-//   await Promise.all(promises);
-//   return list;
-// }
-
-// /**
-//  * 给定文件路径，读取内容，检查其中是否包含标签
-//  */
-// export const extractFileTags = async (filePath: string) => {
-//   const content = await readFileContent(filePath);
-//   return extractTagsFromContent(content) || [];
-// };
-
-// /**
-//  * 从文件内容中获得文件标题，一般为文件的第一行，以 # 开头
-//  * @param {string} filePath
-//  */
-// export async function getFileTitle(filePath: string) {
-//   const content = await readFileContent(filePath);
-//   return extractTitleFromContent(content);
-// }
 
 export function isExcluded(filePath: string) {
   if (/node_modules/.test(filePath)) {
